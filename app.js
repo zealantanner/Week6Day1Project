4adbe9b905479096ca405827275a92a97efff982
zealantanner <zealantanner@gmail.com>
2021-09-23 03:00:47 -0600

    Commit 1

---
+++ app.js
@@ -0,0 +1,103 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+
+
+$(storeItems).each(function(item){
+	if (this.inStock){
+		$('#products').append(`
+
+            <div class="product">
+                <span class="price">${this.name}</span>
+                <span class="name">\$${this.price}</span>
+                <span class="details">${this.details}</span>
+            </div>
+
+        `)
+    }
+});
+
+
+
+
+
+
+
+let button = document.getElementById('darkModeButton');
+
+
+
+function toggleDark(a){button.addEventListener(a, function(){
+	$('body').toggleClass('dark');
+})}
+
+toggleDark('click'); //normal mode
+
+
+// toggleDark('mousemove'); //seizure mode
